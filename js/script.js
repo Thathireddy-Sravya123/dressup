8d79eca6e8295d1ff55160b9d3b7e7a2e0a91f6c
Thathireddy-Sravya123 <120320023+Thathireddy-Sravya123@users.noreply.github.com>
2025-10-07 12:58:07 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+  $(".twentytwenty-container").twentytwenty({
+    default_offset_pct: 0.5, // initial slider position
+    orientation: 'horizontal' // horizontal or vertical
+  });
+});
